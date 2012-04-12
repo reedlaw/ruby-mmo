85c3cef3aa0176951a7f726783c6fb982625cdde
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2012-04-13 02:59:57 +0530

    Araknus: the greedy one

---
+++ players/araknus.rb
@@ -0,0 +1,97 @@
+module Araknus
+  def move
+    if hidden?
+      if full_health?
+        come_out
+        [:attack, enemy]
+      else
+        [:rest]
+      end
+    else
+      if in_danger?
+        hide
+      elsif winning? && (health < 100)
+        [:rest]
+      else
+        [:attack, enemy]
+      end
+    end
+  end
+
+  def hidden?
+    !Game.world[:players].include?(self)
+  end
+
+  # Hide from everyone
+  def hide
+    if Game.world[:players].size != 1
+      Game.world[:players].reject! {|p| p == self}
+    end
+    [:rest]
+  end
+
+  # Come out in middle of field
+  def come_out
+    if hidden?
+      Game.world[:players] << self
+    end
+  end
+
+  def enemy
+    monster || dying_player || weakest
+  end
+
+  def monster
+    rat
+  end
+
+  def rat
+    rats = players.select{|p| p.to_s == 'rat' && p.alive}
+    rats.slice (rats.size/2)
+  end
+
+  def most_experienced
+    players.max { |a,b| experience(a) <=> experience(b) }
+  end
+
+  def dying_player
+    players.find { |p| dying?(p) }
+  end
+
+  def weakest
+    players.min { |a,b| health(a) <=> health(b) }
+  end
+
+  def players
+    Game.world[:players].select { |p| p != self }
+  end
+
+  def in_danger?(p = self)
+    health(p) <= 70
+  end
+
+  def dying?(p = self)
+    health(p) <= 30
+  end
+
+  def winning?(p = self)
+    p == most_experienced
+  end
+
+  def full_health?(p = self)
+    health(p) == 100
+  end
+
+  def experience(p = self)
+    p.stats[:experience]
+  end
+
+  def health(p = self)
+    p.stats[:health]
+  end
+
+  def to_s
+    "araknus"
+  end
+end
+
