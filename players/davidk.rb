b10e3fe157c8809d933b31b47fe6779a6298dba1
david karapetyan <dkarapetyan@gmail.com>
2012-04-13 00:08:02 -0700

    better strategic player that detects cheaters that redefine == a.k.a. stealthy guy

--- players/davidk.rb
+++ players/davidk.rb
@@ -9,7 +9,7 @@ module DavidK
 
   def move
     opponent, full_speed = killable_opponent
-    if (stats[:health] >= 90 || full_speed) && !opponent.nil?
+    if (stats[:health] >= 95 || full_speed) && !opponent.nil?
   	  [:attack, opponent]
     else
       [:rest]
@@ -19,22 +19,38 @@ module DavidK
   private
 
   def killable_opponent
-  	all_opponents = Game.world[:players].select{|p| p != self}
+  	all_opponents = Game.world[:players].select {|p| p.to_s != "david karapetyan"}
     if all_opponents.length == 1
       return all_opponents[0], :yes
     end
-  	n = 1
+    n = 1
     possible_opponents = all_opponents.select {|o| can_kill_in_n_hits?(o, n)}
-    # give each attacked player a gang score, higher scores are worse
+    while possible_opponents.empty?
+      n += 1
+      possible_opponents = all_opponents.select {|o| can_kill_in_n_hits?(o, n)}
+    end
     gang_score = Hash.new(0)
-    all_opponents.map {|p| p.class == "Player" && p.move}.select {|m| m && m.length > 1}.map {|m| m[1]}.each {|k| gang_score[k] += 1}
-    # sort possible opponents by gang score and pick the one with the lowest score
-    return possible_opponents.min {|a,b| gang_score[a] <=> gang_score[b]}, nil
+    attackees = all_opponents.map do |p|
+      if p.respond_to?(:move) then
+        player_move = p.move
+        if player_move.length == 2 then
+          player_move
+        else
+          nil
+        end
+      else
+        nil
+      end
+    end.select {|m| m}.map {|m| m[1]}
+    attackees.each {|k| gang_score[k] += 1}
+    if stats[:health] < 100 && gang_score[self] > 0 then
+      return nil, nil
+    end
+    # favor attacking players over monsters
+    sorted_opponents = possible_opponents.sort {|a,b| gang_score[a] <=> gang_score[b]} 
+    return sorted_opponents.select {|p| p.respond_to?(:move)}[0] || sorted_opponents[0], nil
   end
 
-  # @return [BooleanLiteral]
-  # @param player [Object]
-  # @param n [integer]
   def can_kill_in_n_hits?(player, n)
     enemy_stats = player.stats
     points = stats[:strength] - (enemy_stats[:defense] / 2)
