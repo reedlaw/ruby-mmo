4b12cc30e208471bd80fd1cf109c3c5415ffe987
david karapetyan <dkarapetyan@gmail.com>
2012-04-12 20:01:44 -0700

    no more tabs

--- players/davidk.rb
+++ players/davidk.rb
@@ -4,19 +4,19 @@ module DavidK
   end
 
   def move
-  	opponent = killable_opponent
+    opponent = killable_opponent
   	if stats[:health] >= 50 && !opponent.nil?
-    	[:attack, opponent]
+  	  [:attack, opponent]
     else
-    	[:rest]
+      [:rest]
     end
   end
 
   private
 
   def killable_opponent
-  	all_opponents = Game.world[:players].select{|p| p != self}
-  	n = 1
+    all_opponents = Game.world[:players].select{|p| p != self}
+    n = 1
     possible_opponents = all_opponents.select {|o| can_kill_in_n_hits?(o, n)}.sort {|a,b| b.stats[:health] <=> a.stats[:health]}
     #while possible_opponents.empty?
     #	n += 2
@@ -26,9 +26,8 @@ module DavidK
   end
   
   def can_kill_in_n_hits?(player, n)
-  	enemy_stats = player.stats
-  	points = stats[:strength] - (enemy_stats[:defense] / 2)
-  	enemy_stats[:health] <= n * points
+    enemy_stats = player.stats
+    points = stats[:strength] - (enemy_stats[:defense] / 2)
+    enemy_stats[:health] <= n * points
   end
-  
 end
