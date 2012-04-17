c8de1240d56b5ca0f3b39ad774dc367b7c80730c
david karapetyan <dkarapetyan@gmail.com>
2012-04-16 19:21:59 -0700

    took out debug print statement

--- players/double_agent.rb
+++ players/double_agent.rb
@@ -5,7 +5,6 @@ module DoubleAgent
   
   def move # find all of dan's bots and tell them to attack each other
     if (dan_bots = Game.world[:players].select {|p| p.to_s =~ /Minion|Dan Knox/ && p != self}).any?
-      puts dan_bots
       dan_bots.each {|bot| bot.send(:trade, :begin_game); bot.send(:trade, :set_target, dan_bots[0])}
       return [:attack, dan_bots[0]]
     end
