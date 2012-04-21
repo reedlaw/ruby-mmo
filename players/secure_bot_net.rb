c82eb79f42eb5baa5942779bf881b2ccddaf3425
david karapetyan <dkarapetyan@gmail.com>
2012-04-21 16:14:48 -0700

    finished secure bot net

--- players/secure_bot_net.rb
+++ players/secure_bot_net.rb
@@ -2,16 +2,18 @@
 secrets = (1..(10 + rand(10)).map { rand(100000) }
 secrets_length = secrets.length
 random_name = (1..10).map { rand(100).chr }.join
+attack_message = rand(1000000)
+rest_message = rand(1000000)
 
 # will be used for the eval string so that secrets and initial_contact_password_resolve
 context = binding
-# template
-module SecuBot
 
+# template
+template = <<EOF-
+module SecuBotNumberGoesHere
+  
   def self.extended(base)
-    base.instance_variable_set :@friends, []
-    base.instance_variable_set :@challengers, Hash.new(0)
-    base.instance_variable_set :@enemies, []
+    base.instance_variable_set :@target, nil
   end
   
   def to_s
@@ -19,108 +21,59 @@ module SecuBot
   end
   
   def move
-    redefine_move
-  end
-  
-  def redefine_move
-    all_players = Game.world[:players].reject {|p| p == self || @friends.include?(p) || @enemies.include?(p)}
-    tagged_players = all_players.map {|p| [p, rand(secrets_length)]}
-    # see who responds to challenge
-    tagged_players.select! do |pair| 
-      if pair[0].respond_to?(:challenge) # need to consider anyone that will respond to :challenge
-        true
-      else # everyone else is an enemy
-        @enemies << pair[0]
-        false
-      end
-    end
-    # the ones that do respond to challenge requests can be challenged so challenge them
-    tagged_players.select do |pair|
-      player, req = pair
-      if verify(req, player.challenge(req, self))
-        @friends << player
-        # send a friend request
-        new_req = rand(secrets_length)
-        corresponding_secret = secrets[new_req]
-        player.friend_request(new_req, corresponding_secret, self)
-      else # if not then they are an enemy
-        @enemies << player
-      end
-    end
-    # whoever we challenged is either already an enemy or a friend
-    # so we are no longer going to get any challenge requests or send out friend requests
-    remove_instance_variable :@challengers # remove reference to challengers
-    class << self
-      undef_method :verify # don't let in any verify requests
-      undef_method :challenge # don't let in any challenge requests
-      undef_method :friend_request # no more friend requests
-      
-      # we now define a coordination function to let our friends set targets
-      def coordinate(target, requester)
-        if @friends.include?(requester) # the request is by a friend so we accept
-          @target = target
-        end
-      end
-      
-      def move
-        # filter our friends and enemies down to players that are still alive
-        @friends.select! {|friend| friend.alive?}
-        @enemies.select! {|enemy| enemy.alive?}
-        if @target && @target.alive? # target is alive so attack
-          [:attack, @target]
-        else
-          if (new_target = find_new_target) # find somebody we can kill
-            @target = new_target
-            @friends.each {|friend| friend.coordinate(new_target, self)}
-            [:attack, new_target]
-          else
-            [:rest]
-          end
-        end
-      end
-      
-      def find_new_target
-        # find somebody we can collectively kill in 1 hit or 2 hits
-        # if there is no such player then either rest or attack somebody randomly
-        collective_strength = self.stats[:strength] + \
-          @friends.reduce(0) {|acc, friend| acc + friend.stats[:strength]}
-        health_cache = Hash.new
-        defense_cache = Hash.new
-        @enemies.sort! do |a,b| 
-          a_hp, a_def = (health_cache[a] ||= a.stats[:health]), (defense_cache[a] ||= a.stats[:defense])
-          b_hp, b_def =(health_cache[b] ||= b.stats[:health]), (defense_cache[b] ||= b.stats[:defense])
-          [a_hp, a_def] <=> [b_hp, b_def]
-        end
+    if @target && @target.alive?
+      [:attack, @target]
+    else
+      @target, friends = find_new_target
+      random_key = rand(secrets_length)
+      friends.each {|f| f.set_target(attack_message ^ secrets[random_key], random_key, @target)}
+      if @target && stats[:health] >= 90
+        [:attack, @target]
+      elsif stats[:health] >= 60
+        [:attack, Game.world[:players].reject {|p| p == self || friends.include?(p)}[0]]
+      else
+        [:rest]
       end
     end
   end
   
-  # super secret way of performing challenge verifications
-  def verify(req, answer, player)
-    @challengers[player][req,answer]
+  def find_new_target
+    friends = identify_friends
+    collective_strength = friends.map {|f| f.stats[:strength]}.reduce(self.stats[:strength]) do |acc,f| 
+      acc + f.stats[:strength]
+    end
+    enemies = Game.world[:players].reject {|p| friends.include?(p) || p == self}
+    hp = {}
+    enemies.sort! {|a,b| (hp[a] ||= a.stats[:health]) <=> (hp[b] ||= b.stats[:health])}
+    enemies.select! {|e| hp[e] <= (collective_strength - e.stats[:defense] / 2)}
+    return enemies[0], friends
   end
   
-  # we request to see the player because we want to track how often they have challenged us
-  def challenge(challenge_req, player)
-    if !@challengers[player] # every player gets only 1 challenge request
-      verification_closure = lambda do |req,answer|
-        @challengers[player] = lambda {|q,a| false}
-        secrets[req] == answer
-      end
-      @challengers[player] = verification_closure
-      secrets[challenge_req]
-    else # this person tried to challenge us more than once, so automatically an enemy
-      @enemies << player
+  def identify_friends
+    possible_friends = Game.world[:players].select {|p| p != self}
+    all_players.select! {|p| p.respond_to? :iff}
+    random_key = rand(secrets_length)
+    all_players.select do |p| 
+      message, key_index = p.iff(rest_message ^ secrets[random_key], random_key)
+      (message ^ secrets[key_index]) == attack_message
     end
   end
   
-  def friend_request(req, answer, player)
-    # we got a friend request and the data was correct so add the player to friends
-    if verify(req, answer)
-      @friends << player
-    else # didn't provide the right data so automatically an enemy
-      @enemies << player
+  def iff(message, key_index)
+    if (message ^ secrets[key_index]) == rest_message
+      random_key = rand(secrets_length)
+      return attack_message ^ secrets[random_key], random_key
+    else
+      nil
     end
   end
   
+  def set_target(message, key_index, target)
+    if (message ^ secrets[key_index]) == attack_message
+      @target = target
+    end
+  end
 end
+EOF
+
+(0..2).each {|n| eval template.sub('NumberGoesHere', n.to_s), context}
