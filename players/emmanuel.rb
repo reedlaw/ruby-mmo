2e6b8f0bba9fe6dad1b519693312cb4de146e2cd
Emmanuel Kaunitz <ekaunitz@gmail.com>
2014-03-29 22:10:14 -0700

    added logic to fight efficiently, wins about 15% of the time. Also small bug fix in Jax module, seems to fix error of nil player but I need to test more to be sure

--- players/emmanuel.rb
+++ players/emmanuel.rb
@@ -5,16 +5,57 @@ module Emmanuel
 	end
 
 	def move	
-		p monsters.class
-		sleep 0.5
-		[:attack, monsters.slice(monsters.length-1)]
+		# sleep 0.5
+		if hp_danger? && opponents.length >= 5 || top_3
+			[:rest]
+		else
+			[:attack, attack_sequence]
+		end
 	end
 
 	private 
 
 	# find players with lowest stats, pick off the weak
-	def lowest_stats
-		players.min_by {|opponents| opponents[:experience]}
+	def rest_conditions
+		hp_danger? && opponents.length >= 5 || top_3
+	end
+
+	def attack_sequence
+		can_kill ? can_kill : leader
+	end
+
+	def hp_danger?
+		self.stats[:health] < 90
+	end
+
+	def top_3
+		
+		top_3_xp.include?(self.to_s)
+	end
+
+	def top_3_xp
+	 opponents_array = all_players.sort { |a, b| b.stats[:experience] <=> a.stats[:experience] }.take(opponents.length/2)
+	 strings = opponents_array.map(&:to_s)
+	end
+
+	def leader
+		opponents.max_by { |p| p.stats[:experience]}
+	end
+	
+	def can_kill
+		opponents.select{ |p| damage_to_kill(p) }.sample
+	end
+
+	def damage_to_kill(opponent)
+		(self.stats[:strength] - opponent.stats[:defense] / 2) >= opponent.stats[:health]
+	end
+
+	def lowest_xp
+		opponents.min_by {|opponents| opponents[:experience]}
+	end
+
+	def lowest_hp
+		opponents.min_by {|opponents| opponents[:health]}
 	end	
 
 	def monsters
@@ -23,5 +64,9 @@ module Emmanuel
 
 	def opponents 
 		Game.world[:players].select {|p| p != self}
+	end
+
+	def all_players
+		Game.world[:players]
 	end
 end
